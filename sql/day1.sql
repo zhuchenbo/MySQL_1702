cf421e70f29ca64cccdea6e1286f35e3d16d9801
zhuchenbo <749822145@qq.com>
2017-05-15 17:39:35 +0800

    Update

--- sql/day1.sql
+++ sql/day1.sql
@@ -1,2 +1,26 @@
-SHOW DATABASES ;
---
+SHOW DATABASES;
+
+-- dialect 方言
+
+CREATE DATABASE db_1702;
+
+DROP DATABASE db_1702;
+
+CREATE DATABASE db_1702;
+
+USE db_1702;
+
+SHOW TABLES;
+
+CREATE TABLE t_student (
+  sno    VARBINARY(10), -- varbinary 可变长字符类型
+  sname  VARBINARY(10),
+  gender CHAR(1),
+  age    INT(2)
+);
+
+SELECT *
+FROM t_student;
+
+INSERT INTO t_student
+  VALUE ('2017001', '张三', '男', 18);
