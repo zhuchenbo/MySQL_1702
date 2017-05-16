8178be61a4978241b38d86303320a1cef8ed4737
zhuchenbo <749822145@qq.com>
2017-05-16 11:55:52 +0800

    Update

--- sql/day2.sql
+++ sql/day2.sql
@@ -1,6 +1,8 @@
-DROP TABLE db_1702.student;
+DROP DATABASE IF EXISTS db_1702;
+CREATE DATABASE db_1702;
 
-CREATE TABLE db_1702.student (
+DROP TABLE IF EXISTS db_1702.student;
+CREATE TABLE db_1702.student(
   name VARBINARY(10) COMMENT '姓名',
   intro TEXT COMMENT '简介',
   gender CHAR(2) COMMENT '性别',
@@ -10,7 +12,6 @@ CREATE TABLE db_1702.student (
   dob  DATE COMMENT '出生日期',-- date of birth
   time DATETIME COMMENT '时间'
 );
-
 
 INSERT INTO db_1702.student VALUE ('Tom','dasf','M',19,1.7,123.456,'1997-11-4','2017-5-6 10:39:10');
 
