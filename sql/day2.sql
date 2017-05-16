c9cda3a3dea31b2b699e50be2e674504a6454a79
zhuchenbo <749822145@qq.com>
2017-05-16 10:54:19 +0800

    Update

---
+++ sql/day2.sql
@@ -0,0 +1,18 @@
+DROP TABLE db_1702.student;
+
+CREATE TABLE db_1702.student (
+  name VARBINARY(10),
+  intro TEXT,
+  gender CHAR(2),
+  age INT,
+  height double(3,2),
+  price DECIMAL(6,2),
+  dob  DATE,-- date of birth
+  time DATETIME
+);
+
+
+INSERT INTO db_1702.student VALUE ('Tom','dasf','M',19,1.7,123.456,'1997-11-4','2017-5-6 10:39:10');
+
+SELECT *
+from db_1702.student;
